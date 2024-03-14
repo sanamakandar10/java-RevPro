b18f6deaee8ef32e3644829f4becece767a87ed0
SANA MAKANDAR <sanamcs2019@gmail.com>
2024-03-14 13:32:23 +0530

    Patterns

--- Patterns/src/com/revature/patterns/day2/part1/HollowSquareMatrix.java
+++ Patterns/src/com/revature/patterns/day2/part1/HollowSquareMatrix.java
@@ -2,32 +2,27 @@ package com.revature.patterns.day2.part1;
 
 public class HollowSquareMatrix {
     public static void main(String[] args) {
-        int size=5;
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j <size ; j++) {
-
-                    for (int k = 0; k < 3; k++) {
-                        if (j == 0 || j == size - 1 || k == 0 || k == 2) {
-                            for (int l = 0; l < size; l++) {
-                                System.out.print("* ");
-                            }
-                        } else {
-                            for (int l = 0; l < size; l++) {
-                                if (l == 0 || l == size - 1) {
-                                    System.out.print("* ");
-                                } else {
-                                    System.out.print("  ");
-                                }
-                            }
-                        }
-
-                        System.out.print("  ");
-                    }
-                    System.out.println();
+        int size = 5;
+        int rows = 3;
+        int cols = 3;
+        int r=1;
+        for (int j = 0; j < size; j++) {
+            int c = 1;
+            for (int s = 0; s < size; s++) {
+                if(s==size-1 && c<cols) {
+                    c++;
+                    s = -1;
                 }
-                System.out.println();
+                if (j == 0 || j == size - 1 || s == 0 || s == size - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
             }
-
+            if(j==size-1 && r<rows){
+                j=-1;
+                r++;
+            }
+            System.out.println();
         }
     }
+}
